08fea02df0a6f84abe11feb14169bd7c1f3d5d7e
Antoni Kowalewski <kowalewski.antek@gmail.com>
2021-12-05 23:35:38 +0100

    Fill Login Page

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import styles from './Login.module.scss';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
 
 const Login = () => (
 
-  <div className={styles.component}>
-    <h2>Login view</h2>
+  <div>
+    <Paper className={styles.component}>
+      <form>
+        <TextField className={styles.loginput} id="standard-basic" label="Nick" variant="standard" />
+        <TextField className={styles.loginput} id="standard-basic" label="Password" variant="standard" />
+      </form>
+      <Button className={styles.logbutton} variant="contained" color="primary" to={`${process.env.PUBLIC_URL}/panel/login`}>Submit</Button>
+    </Paper>
   </div>
 );
 
