7bf5e7bbd6ff61a08b10e24bb60024c5d278ec56
Antoni Kowalewski <kowalewski.antek@gmail.com>
2021-12-13 14:36:00 +0100

    Add Waiter API

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MainLayout from './components/layout/MainLayout/MainLayout';
 import Dashboard from './components/views/Dashboard/Dashboard';
 import Login from './components/views/Login/Login';
 import Tables from './components/views/Tables/Tables';
-import Waiter from './components/views/Waiter/Waiter';
+import Waiter from './components/views/Waiter/WaiterContainer';
 import Kitchen from './components/views/Kitchen/Kitchen';
 import { StylesProvider } from '@material-ui/styles';
 import { createTheme, ThemeProvider } from '@material-ui/core/styles';
